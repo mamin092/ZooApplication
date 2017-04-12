67a350b3be01a3d2484b5d2c57a77076b42999ed
mamin092 <mnurulamin092@gmail.com>
2017-04-12 10:21:56 +0600

    emote add origin git@github.com:mamin092/ZooApp.git

---
+++ ZooApp.MvcClient/Controllers/Animal1Controller.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZooApp.Models;
+using ZooApp.Services;
+using ZooApp.ViewModels;
+
+namespace ZooApp.MvcClient.Controllers
+{
+    public class Animal1Controller : Controller
+    {
+        AnimalService service = new AnimalService();
+        // GET: Animal1
+
+        public ActionResult Index()
+        {
+          var  viewAnimals=service.GetAnimals();
+            return View(viewAnimals);
+        }
+        public ActionResult Details(int id)
+        {
+           ViewAnimal animal = service.GetAnimal(id);
+            return View(animal);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Animal animal)
+        {
+           bool saved= service.Save(animal); 
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Animal animal = service.GetDbAnimal(id);
+            return View(animal);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Animal animal)
+        {
+            var update = service.Update(animal);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Animal animal)
+        {
+            var  delete=service.Delete(animal);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ZooApp.ViewModels/ViewAnimal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooApp.ViewModels
+{
+    public class ViewAnimal
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Food { get; set; }
+        public string Origin { get; set; }
+        public double Quantity { get; set; }
+    }
+}
